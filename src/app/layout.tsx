51b418e37cd70dcd7be6d0161bd8c986802757ad
baopowerpoint <baopowerpoint@gmail.com>
2025-01-30 19:28:56 +0700

    ''

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono, Raleway } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const jbSans = JetBrains_Mono({
+  variable: "--font-jb-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const ralewaySans = Raleway({
+  variable: "--font-raleway-mono",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${jbSans.variable} ${ralewaySans.variable} antialiased`}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,8 +13,39 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          "1": "var(--primary-1)",
+          "2": "var(--primary-2)",
+          "3": "var(--primary-3)",
+          "4": "var(--primary-4)",
+        },
+        secondary: {
+          "1": "var(--secondary-1)",
+          "2": "var(--secondary-2)",
+          "3": "var(--secondary-3)",
+          "4": "var(--secondary-4)",
+        },
+        neutrals: {
+          "1": "var(--neutrals-1)",
+          "2": "var(--neutrals-2)",
+          "3": "var(--neutrals-3)",
+          "4": "var(--neutrals-4)",
+          "5": "var(--neutrals-5)",
+          "6": "var(--neutrals-6)",
+          "7": "var(--neutrals-7)",
+          "8": "var(--neutrals-8)",
+        },
+        fontFamily: {
+          jetbrains: ["var(--font-inter)"],
+          "space-grotesk": ["var(--font-space-grotesk)"],
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
